1ae61a23eaaea06ae53a6501822e1d831eb43dc1
sjones4215 <bluesfansince2013@gmail.com>
2021-01-04 18:43:09 -0600

    description added to boards controller.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -46,6 +46,6 @@ class BoardsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def board_params
-      params.require(:board).permit(:title, :image, :user_id)
+      params.require(:board).permit(:title, :description, :image, :user_id)
     end
 end
